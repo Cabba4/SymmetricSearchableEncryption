a13ff005108b7c3be7c6d4f587ab3348e0fa0ea5
Cabba4 <anmolsfs@gmail.com>
2025-05-03 17:23:13 +0300

    sonarqube changes

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import CSRFProtect
 import os
 import hashlib
 from database import encrypt_and_store_file, search_encrypted_data, clear_tables # Functions from database.py
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+csrf = CSRFProtect(app)
 
 app.config['UPLOAD_FOLDER'] = 'textfiles'
 app.config['ALLOWED_EXTENSIONS'] = {'txt'}
+
+INDEX_PAGE = 'index.html'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(INDEX_PAGE)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return redirect(request.url)
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
+    up_file = request.files['file']
+    if up_file and allowed_file(up_file.filename):
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], up_file.filename)
+        up_file.save(filename)
 
         # Generate KSKE key when file is uploaded
         ip_address = request.remote_addr
@@ -50,9 +55,9 @@ def search():
     results = search_encrypted_data(query, kske_key)
 
     if not results:
-        return render_template('index.html', message="Word not found.")
+        return render_template(INDEX_PAGE, message="Word not found.")
 
-    return render_template('index.html', results=results)
+    return render_template(INDEX_PAGE, results=results)
 
 @app.route('/clear_tables', methods=['POST'])
 def clear_database():
